c3652707d1f1f2889bdc017183bcc23609b69065
Thomas Stapleton <tst@tradeshift.com>
2020-06-11 17:18:53 -0700

    add sidebar with player list and game actions

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { v4 } from 'uuid';
+import deer from './deer.svg';
 import './Home.css';
 
 export default function Home() {
 	const gameId = v4();
 	return (
 		<div className="Home">
+			<img src={deer} alt="logo" />
 			<h1>Agilitious</h1>
 			<Link to={`/games/${gameId}`}>Start a new game</Link>
 		</div>
---
+++ src/SidebarHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import deer from './deer.svg';
+import './SidebarHeader.css';
+
+export default function SidebarHeader() {
+	return (
+		<div className="SidebarHeader">
+			<img src={deer} alt="logo" />
+			<h1>Agilitious</h1>
+		</div>
+	)
+};
