8dcc2513bcca02909d748049788b7371a4f7136e
Thomas Stapleton <tst@tradeshift.com>
2020-06-11 19:38:21 -0700

    connect to websocket

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import Sidebar from './Sidebar';
 import Table from './Table';
+
 import './Game.css';
 
 export default function Game() {
 	const { gameId } = useParams();
 	console.log(`gameId: ${gameId}`);
+
+	const joinGame = () => sendJsonMessage({ type: 'JOIN_GAME', gameId });
+	const handleMessage = (event) => console.log(event);
+
+	const [socketUrl, setSocketUrl] = useState('ws://localhost:8000');
+
+	const {
+		sendJsonMessage,
+		lastJsonMessage,
+		readyState,
+	} = useWebSocket(socketUrl, {
+		onOpen: joinGame,
+		onMessage: handleMessage,
+		shouldReconnect: (closeEvent) => true,
+	});
 
 	return (
 		<div className="Game">
