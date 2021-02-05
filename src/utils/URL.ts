d36da55c859e581659166c15238ea66a238d3a0d
Thomas Stapleton <tst@tradeshift.com>
2021-02-05 12:50:30 -0800

    use environment variables for talking to server

--- src/utils/URL.ts
+++ src/utils/URL.ts
@@ -3,5 +3,7 @@ import { AvatarId } from '../types/player';
 
 export const getAvatarURLForPlayer = (player: Player) => getAvatarURLForAvatarId(player.avatarId);
 
+const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+
 export const getAvatarURLForAvatarId = (avatarId: AvatarId) =>
-	`${window.location.origin}/api/avatar/${avatarId}`;
+	`${protocol}://${process.env.REACT_APP_BASE_URL}/api/avatar/${avatarId}`;
